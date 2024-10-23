fa0b87552705b52866b0e4556078fa6915b8eca1
Hassan El Mghari <hassan4709@gmail.com>
2024-10-23 10:31:35 -0400

    simplified streaming

--- pages/api/together.ts
+++ pages/api/together.ts
@@ -1,40 +1,26 @@
-import Together from 'together-ai';
+import Together from "together-ai";
 
 if (!process.env.TOGETHER_API_KEY) {
-  throw new Error('Missing env var from Together.ai');
+  throw new Error("Missing env var from Together.ai");
 }
-
-const together = new Together({
-  auth: process.env.TOGETHER_API_KEY,
-});
+const together = new Together();
 
 const handler = async (req: Request): Promise<Response> => {
   const { prompt } = (await req.json()) as { prompt?: string };
 
   if (!prompt) {
-    return new Response('No prompt in the request', { status: 400 });
+    return new Response("No prompt in the request", { status: 400 });
   }
 
-  const stream = await together.inference(
-    'mistralai/Mixtral-8x7B-Instruct-v0.1',
-    {
-      messages: [
-        {
-          role: 'user',
-          content: `${prompt} Reply in markdown.`,
-        },
-      ],
-      max_tokens: 512,
-      stream_tokens: true,
-    }
-  );
-
-  return new Response(stream as ReadableStream, {
-    headers: new Headers({
-      'Cache-Control': 'no-cache',
-    }),
+  const runner = together.chat.completions.stream({
+    model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+    messages: [
+      { role: "user", content: "tell me about nyc, respond in markdown" },
+    ],
   });
+
+  return new Response(runner.toReadableStream());
 };
 
-export const config = { runtime: 'edge' };
+export const config = { runtime: "edge" };
 export default handler;
